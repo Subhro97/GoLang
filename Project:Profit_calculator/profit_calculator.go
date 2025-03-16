da59da2767ea54bffac59cc2c0c42f2c7cd5670d
Subhrojit Mondal <subhromond@gmail.com>
2025-03-16 20:42:49 +0530

    Refactors Task Go Bank

--- Project:Profit_calculator/profit_calculator.go
+++ Project:Profit_calculator/profit_calculator.go
@@ -2,27 +2,55 @@
 
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
-	var revenue, expenses, taxRate float64
 
-	revenue = userEnteredValue("Revenue: ₹")
-	expenses = userEnteredValue("Expenses: ₹")
-	taxRate = userEnteredValue("Tax Rate: ")
+	revenue, err := userEnteredValue("Revenue: ₹")
+
+	if err != nil {
+		panic(err)
+	}
+
+	expenses, err := userEnteredValue("Expenses: ₹")
+
+	if err != nil {
+		panic(err)
+	}
+
+	taxRate, err := userEnteredValue("Tax Rate: ")
+
+	if err != nil {
+		panic(err)
+	}
 
 	earningsBeforeTax, earningsAfterTax, ratio := calculations(revenue, expenses, taxRate)
+
+	calErr := writeCalculationToFile(earningsBeforeTax, earningsAfterTax, ratio)
+
+	if calErr != nil {
+		panic(calErr)
+	}
 
 	fmt.Printf("Earnings Before Tax: ₹%v\n", earningsBeforeTax)
 	fmt.Printf("Earnings After Tax: ₹%v\n", earningsAfterTax)
 	fmt.Printf("Ratio: %0.2f\n", ratio)
 }
 
-func userEnteredValue(text string) (value float64) {
+func userEnteredValue(text string) (value float64, err error) {
 	fmt.Print(text)
 	fmt.Scan(&value)
 
-	return value
+	if value <= 0 {
+		fmt.Println("Please Enter value greater than 0")
+		return 0, errors.New("value is negative")
+	}
+
+	return value, nil
 }
 
 func calculations(revenue, expenses, taxRate float64) (ebt, eat, ratio float64) {
@@ -31,4 +59,16 @@ func calculations(revenue, expenses, taxRate float64) (ebt, eat, ratio float64) 
 	ratio = ebt / eat
 
 	return ebt, eat, ratio
+}
+
+func writeCalculationToFile(ebt, eat, ratio float64) error {
+	calculationText := fmt.Sprintf("EBT: ₹%0.2f\nEAT: ₹%0.2f\nRatio: ₹%0.2f", ebt, eat, ratio)
+
+	err := os.WriteFile("Calculations.txt", []byte(calculationText), 0644)
+
+	if err != nil {
+		fmt.Println("Failed to write to File.")
+		return err
+	}
+	return nil
 }
